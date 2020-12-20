1d2052e2c7f399b57dec729521f6a2baf69f34fa
Anandu-kichu <anandukch@gmail.com>
2020-12-20 16:03:38 +0530

    pattern file added

---
+++ pattern.py
@@ -0,0 +1,13 @@
+
+
+
+def pattern(lines):
+    n=1
+    for i in range(1,lines+1):
+        for j in range(1,i+1):
+            print(n,end="")
+            n+=1
+        for j in range(1,i):
+            print(n,end='')
+            n-=1
+        print()
